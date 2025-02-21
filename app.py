c8059228f717d8798f2a4f61d18df10c8070e4a1
Latino Solutions <thankyoucovidnowimatrader@gmail.com>
2025-02-21 18:44:04 +0100

    update manual a 20k - name upd

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ import gspread
 # 1) Configuración general de la página de Streamlit
 # ------------------------------------------------------
 st.set_page_config(
-    page_title="Bruno's QJ",
+    page_title="Bru's QJ",
     layout="wide",
     initial_sidebar_state="expanded"
 )
